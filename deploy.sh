6dac9942e432771f2acfceae2413662cc9d8faaa
Haixiang <haixiang6123@gmail.com>
2019-11-13 18:56:38 -0800

    Update remote repo url

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:Haixiang6123/bio-guo.git master:gh-pages
+ git push -f git@github.com:Haixiang6123/si-kun.git master:gh-pages
 
 cd -
